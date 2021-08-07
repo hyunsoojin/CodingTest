8a304d26f67351ee6918021a3a92e5eea6b6a419
hyunsoojin <45657479+hyunsoojin@users.noreply.github.com>
2021-08-07 15:16:50 +0900

    Add files via upload
    https://programmers.co.kr/learn/courses/30/lessons/1844

--- GameMapShortest.java
+++ GameMapShortest.java
@@ -1,43 +1,55 @@
 package programmers;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class GameMapShortest {
 
+	
+	
 	static class Solution {
-		public void dfs(int x, int y, int[][] maps, boolean[][] visited, int sum) {
-			if(x < 0 || x >= maps.length || y < 0 || y >= maps[0].length || visited[x][y] || maps[x][y] == 0)
-				return;
-			
-			visited[x][y] = true;
-			
-			if(maps[x][y] != 1 && maps[x][y] >= sum) return;
-			else maps[x][y] += sum;
-			
-			dfs(x+1, y, maps, visited, maps[x][y]);
-			dfs(x , y+1, maps, visited, maps[x][y]);
-			dfs(x-1, y, maps, visited, maps[x][y]);
-			dfs(x, y-1, maps, visited, maps[x][y]);
-			
+		class Point{
+			int x, y;
+			public Point(int x, int y) {
+				this.x = x;
+				this.y = y;
+			}
 		}
-		
 	    public int solution(int[][] maps) {
-	        int answer = 0;
 	        boolean[][] visited = new boolean[maps.length][maps[0].length];
+	        Queue<Point> q = new LinkedList<>();
+	        q.add(new Point(0, 0));
+	        int[][] d = {
+	        		{0, 1},{-1, 0},{0, -1}, {1, 0}
+	        };
 	        
-	        dfs(0, 0, maps, visited, 0);
+	        while(!q.isEmpty()) {
+	        	Point p = q.poll();
+	        	visited[p.x][p.y] = true;
+	        	
+	        	for(int i=0;i<d.length;i++) {
+	        		if(p.x+d[i][0] < 0 || p.x+d[i][0] >= maps.length || p.y+d[i][1] < 0 || p.y + d[i][1] >= maps[0].length)
+	        			continue;
+	        		if(maps[p.x+d[i][0]][p.y+d[i][1]] == 0 || visited[p.x+d[i][0]][p.y+d[i][1]])
+	        			continue;
+	        		if(maps[p.x+d[i][0]][p.y+d[i][1]] == 1) {
+	        			maps[p.x+d[i][0]][p.y+d[i][1]] = maps[p.x][p.y] + 1;
+	        			q.add(new Point(p.x+d[i][0], p.y+d[i][1]));
+	        		}
+	        	}
+	        }
 	        
-	        if(maps[maps.length-1][maps[0].length-1] <= 1) answer = -1;
-	        else answer = maps[maps.length-1][maps[0].length-1];
-	        
-	        return answer;
+	        return maps[maps.length-1][maps[0].length-1];
 	    }
 	}
 	
 	public static void main(String[] args) {
 		int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
-		
+		for(int i=0;i<maps.length;i++) System.out.println(Arrays.toString(maps[i]));
 		Solution s = new Solution();
-		System.out.println(s.solution(maps)); 
-
+		System.out.println(s.solution(maps));
+		// 테케 1, 5, 6, 10 17 효율성 2, 3, 4
 	}
 
 }
